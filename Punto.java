7634f52e6b53f00dd22104bfcbf407165c2252c3
marcellom-png <marcello.maccioni@iisbuonarrotivolta.it>
2025-11-05 20:09:50 +0100

    Add files via upload

---
+++ Punto.java
@@ -0,0 +1,21 @@
+
+package testpunto;
+
+
+public class Punto {
+    public double x;
+    public double y;
+    
+    Punto(){
+       
+    }
+    
+    Punto(double x, double y){
+        
+    }
+ 
+    
+    public void visualizza (){
+        System.out.println("");
+    }
+}
